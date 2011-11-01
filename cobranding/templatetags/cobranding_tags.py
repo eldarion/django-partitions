3f60786dcf3b29fbda17fabf9920f0be078cfa6c
Donald Stufft <donald.stufft@gmail.com>
2011-11-01 17:59:15 -0400

    switched the template tag to use the cobranding filter function

--- cobranding/templatetags/cobranding_tags.py
+++ cobranding/templatetags/cobranding_tags.py
@@ -1,13 +1,13 @@
 from django import template
 
-from cobranding.brands import registry
+from cobranding.brands import filter_by_cobrand
 
 
 register = template.Library()
 
 
 class CobrandFilterNode(template.Node):
-    
+
     @classmethod
     def handle_token(cls, parser, token):
         bits = token.split_contents()
@@ -19,19 +19,17 @@ class CobrandFilterNode(template.Node):
             queryset = bits[1],
             hostname = bits[3]
         )
-    
+
     def __init__(self, queryset, hostname):
         self.queryset = template.Variable(queryset)
         self.hostname = template.Variable(hostname)
-    
+
     def render(self, context):
         var_name = self.queryset.var
         queryset = self.queryset.resolve(context)
         hostname = self.hostname.resolve(context)
-        app_model = "%s.%s" % (queryset.model._meta.app_label, queryset.model._meta.object_name)
-        expression = registry.expression_for(hostname, app_model)
-        if expression:
-            context[var_name] = queryset.filter(expression)
+
+        context[var_name] = filter_by_cobrand(queryset, hostname)
         return ""
 
 
@@ -40,7 +38,7 @@ def cobrand_filter(parser, token):
     """
     Usage::
         {% cobrand_filter queryset using request.get_host %}
-    
+
     Filters the queryset by the given hostname using rules defined in
     cobranding.brands.registry
     """
