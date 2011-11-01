a01f8e002b993e5b075bc353ca626b74b8b1d5d2
Patrick Altman <paltman@gmail.com>
2011-11-01 16:56:30 -0500

    Add some validation

--- cobranding/brands.py
+++ cobranding/brands.py
@@ -1,17 +1,21 @@
+from django.conf import settings
+
+
 class Registry(object):
     
     def __init__(self):
         self._brands = {}
     
     def register(self, domain, app_model, expression):
-        # @@@ Validation: what if it is already registered?
-        # @@@ Validation: what if app_model is invalid for INSTALLED_APPS
-        # @@@ Validation: what if expression is not a valid Q expression
-        if domain not in self._brands:
-            self._brands[domain] = {}
         if not isinstance(app_model, basestring):
             app_model = "%s.%s" % (app_model._meta.app_label, app_model._meta.object_name)
-        self._brands[domain][app_model] = expression
+        
+        if domain in self._brands:
+            raise Exception("'%s' is already registered." % domain)
+        if app_model.split(".")[0] not in settings.INSTALLED_APPS:
+            raise Exception("'%s' is not in INSTALLED_APPS" % app_model.split(".")[0])
+        
+        self._brands[domain] = {app_model: expression}
     
     def expression_for(self, domain, app_model):
         return self._brands.get(domain, {}).get(app_model)
