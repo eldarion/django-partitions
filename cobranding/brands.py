8bcd5020aa6debf047717f2915f8550bc4b20d30
Donald Stufft <donald.stufft@gmail.com>
2011-11-01 17:54:41 -0400

    added a utility function to filter by a cobrand

--- cobranding/brands.py
+++ cobranding/brands.py
@@ -2,21 +2,21 @@ from django.conf import settings
 
 
 class Registry(object):
-    
+
     def __init__(self):
         self._brands = {}
-    
+
     def register(self, domain, app_model, expression):
         if not isinstance(app_model, basestring):
             app_model = "%s.%s" % (app_model._meta.app_label, app_model._meta.object_name)
-        
+
         if domain in self._brands:
             raise Exception("'%s' is already registered." % domain)
         if app_model.split(".")[0] not in settings.INSTALLED_APPS:
             raise Exception("'%s' is not in INSTALLED_APPS" % app_model.split(".")[0])
-        
+
         self._brands[domain] = {app_model: expression}
-    
+
     def expression_for(self, domain, app_model):
         return self._brands.get(domain, {}).get(app_model)
 
@@ -26,3 +26,9 @@ registry = Registry()
 
 def register(domain, app_model, expression):
     registry.register(domain, app_model, expression)
+
+
+def filter_by_cobrand(queryset, by):
+    app_model = "%s.%s" % (queryset.model._meta.app_label, queryset.model._meta.object_name)
+    expression = registry.expression_for(by, app_model)
+    return queryset.filter(expression) if expression is not None else queryset
